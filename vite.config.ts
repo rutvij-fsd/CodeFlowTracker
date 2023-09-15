0bfe7e1993d70c5c46bde1f91c426b7c716e8df1
Rutvij Patel <rutvij.patel@embifi.in>
2023-09-15 11:55:40 +0530

    feat: configure Vite for React and Buffer integration
    - Updated `vite.config.ts`:
      - Imported necessary modules for Vite and React.
      - Defined global `Buffer` property.
      - Configured Vite plugins, build options, and global definitions.
    
    - Modified `tsconfig.json`:
      - Added references to `tsconfig.node.json`.
      - Specified types for Vite client and Node.
    
    Note: Ensured compatibility between Vite, React, and Buffer.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { Buffer } from 'buffer'
+
+Object.defineProperty(globalThis, 'Buffer', {
+  value: Buffer,
+});
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@babel/parser', '@babel/traverse'],
+    },
+  },
+  define: {
+    'process.env': {},
+    global: 'window',
+    Buffer: [ 'buffer', 'Buffer' ]
+  }
 })
