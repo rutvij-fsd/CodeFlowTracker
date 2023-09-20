bc34e470c3262a18d122d8ec58f4890c0073318f
rutvij-fsd <rutvijpatel414@gmail.com>
2023-09-20 22:27:18 +0530

    Enhance display.ts with graph visualization logic
    - Removed the `displayResults` function.
    - Introduced the `displayGraphWithCode` function to visualize code occurrences as a graph.
    - Implemented sorting logic for occurrences based on file and location.
    - Utilized a node map to efficiently manage graph nodes.
    - Defined valid context connections for determining graph edges.
    - Implemented edge-setting logic based on valid context connections.
    - Enhanced graph edge determination with specific conditions for function calls and parameters.

--- src/utils/display.ts
+++ src/utils/display.ts
@@ -1,27 +1,106 @@
-import { Graph } from 'graphlib';
-import { Occurrence } from './types';
-import { analyzeFunctionUsage } from './codeAnalysis';
+import { Graph } from "graphlib";
+import { Occurrence } from "./types";
 
-export function displayResults(occurrences: Occurrence[]): void {
-    console.log("Occurrences of the identifier:\n");
+export async function displayGraphWithCode(
+  graph: Graph,
+  occurrences: Occurrence[]
+): Promise<Graph> {
+  occurrences.sort((a, b) => {
+    if (a.file === b.file) {
+      return (
+        a.location.start.line - b.location.start.line ||
+        a.location.start.column - b.location.start.column
+      );
+    }
+    return a.file.localeCompare(b.file);
+  });
 
-    occurrences.forEach(occurrence => {
-        console.log(`- File: ${occurrence.file}`);
-        console.log(`  Function: ${occurrence.function}`);
-        console.log(`  Location: Line ${occurrence.location.start.line}, Column ${occurrence.location.start.column}`);
-        console.log(`  Code Block:\n${occurrence.codeBlock}\n`);
-    });
-}
+  let nodeMap = new Map();
 
-export async function displayGraphWithCode(graph: Graph, occurrences: Occurrence[]): Promise<Graph> {
+  occurrences.forEach((occurrence, index) => {
+    const key = `${occurrence.file}-${occurrence.function}-${occurrence.codeBlock}-${occurrence.context}`;
+    if (!nodeMap.has(key)) {
+      nodeMap.set(key, index);
+      graph.setNode(index.toString(), occurrence);
+    }
+  });
 
-    for (const [key, value] of Object.entries(occurrences)) {
-        graph.setNode(key, value);
+  const setGraphEdge = (source: Occurrence, target: Occurrence) => {
+    const edgeLabel = `${source.context} -> ${target.context}`;
+    graph.setEdge(
+      nodeMap
+        .get(
+          `${source.file}-${source.function}-${source.codeBlock}-${source.context}`
+        )
+        .toString(),
+      nodeMap
+        .get(
+          `${target.file}-${target.function}-${target.codeBlock}-${target.context}`
+        )
+        .toString(),
+      { label: edgeLabel }
+    );
+  };
+
+  for (let source of occurrences) {
+    for (let target of occurrences) {
+      if (
+        source.function !== target.function &&
+        source.context !== "Function Call"
+      )
+        continue;
+
+      type ValidContext =
+        | "Function Parameter"
+        | "Function Call"
+        | "JSX Attribute"
+        | "Import Statement"
+        | "Variable Declaration";
+
+      const validConnections: Record<ValidContext, string[]> = {
+        "Function Parameter": [
+          "Equality or Comparison Check",
+          "Function Call",
+          "Return Statement",
+        ],
+        "Function Call": ["Function Parameter"],
+        "JSX Attribute": ["Hook Declaration"],
+        "Import Statement": ["JSX Element"],
+        "Variable Declaration": [
+          "Equality or Comparison Check",
+          "Function Call",
+          "Return Statement",
+        ],
+      };
+      function getValidConnections(context: string): string[] | undefined {
+        return validConnections[context as ValidContext];
       }
-      for (const [key, value] of Object.entries(occurrences)) {
-        for (const [key2, value2] of Object.entries(value)) {
-          graph.setEdge(key, key2, value2);
+
+      if (getValidConnections(source.context)?.includes(target.context)) {
+        if (
+          source.context === "Function Call" &&
+          target.context === "Function Parameter" &&
+          target.declaration.includes(source.codeBlock)
+        ) {
+          setGraphEdge(target, source);
+          continue;
+        } else if (
+          source.context === "Function Call" &&
+          target.context === "Function Parameter" &&
+          source.codeBlock.includes(target.function)
+        ) {
+          setGraphEdge(source, target);
+        } else if (
+          source.context === "Function Call" &&
+          target.context === "Function Parameter"
+        ) {
+          continue;
         }
+
+        setGraphEdge(source, target);
       }
-    return graph;
+    }
+  }
+
+  return graph;
 }
