fff2a7250b53bdcf93f41ff98a505c5a399538cd
rutvij-fsd <rutvijpatel414@gmail.com>
2023-09-20 22:16:44 +0530

    Refactor axios configuration to use dynamic baseURL
    - Removed hardcoded baseURL for GitHub API.
    - Introduced `getBaseURLToDev` function from `constants/Generals` to dynamically set the baseURL.
    - Added request interceptor to set the baseURL before each request.
    - Enhanced error handling with a rejection in the interceptor.

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -1,13 +1,23 @@
 import axios from 'axios';
-
+import { getBaseURLToDev } from '../constants/Generals';
 const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
 
 
 export const axiosInstance = axios.create({
-  baseURL: "https://api.github.com/repos/rutvij-fsd/test-flow-project/contents/",
+  
   timeout: 10000,
   headers: {
     'Authorization': `token ${GITHUB_TOKEN}`,
     'Accept': 'application/vnd.github.v3.raw'
   }
 });
+
+axiosInstance.interceptors.request.use(
+  async config => {
+    config.baseURL = getBaseURLToDev();
+    return config;
+  },
+  (error: any) => {
+    Promise.reject(error);
+  },
+);
