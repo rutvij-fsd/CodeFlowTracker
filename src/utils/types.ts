d889954834c493dc5fd3ddbac25c9de6a679a99c
rutvij-fsd <rutvijpatel414@gmail.com>
2023-09-20 22:32:38 +0530

    Update types.ts with enhanced type definitions
    - Added a `context` field to the `Occurrence` interface.
    - Made the `declaration` field in the `Occurrence` interface non-optional.
    - Removed duplicate `RepoItem` interface definition.
    - Streamlined the overall type definitions for clarity and consistency.

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,36 +1,37 @@
 export interface AxiosResponse {
-    data: any;
-  }
-  
-  export interface RepoItem {
-    type: string;
-    name: string;
-    path: string;
-  }
-  
-  export interface Occurrence {
-    type: string;
-    location: {
-      start: { line: number; column: number };
-      end: { line: number; column: number };
-    };
-    file: string;
-    function: string;
-    codeBlock: string;
-    declaration?: string;
-  }
-  
-  export interface FunctionUsage {
-    file: string;
-    location: {
-      start: { line: number; column: number };
-      end: { line: number; column: number };
-    };
-    callCode: string;
-  }
-  
-  export interface RepoItem {
-    type: string;
-    name: string;
-    path: string;
-  }
+  data: any;
+}
+
+export interface RepoItem {
+  type: string;
+  name: string;
+  path: string;
+}
+
+export interface Occurrence {
+  type: string;
+  location: {
+    start: { line: number; column: number };
+    end: { line: number; column: number };
+  };
+  file: string;
+  function: string;
+  codeBlock: string;
+  declaration: string;
+  context: string;
+}
+
+export interface FunctionUsage {
+  file: string;
+  location: {
+    start: { line: number; column: number };
+    end: { line: number; column: number };
+  };
+  callCode: string;
+}
+
+export interface RepoItem {
+  type: string;
+  name: string;
+  path: string;
+}
