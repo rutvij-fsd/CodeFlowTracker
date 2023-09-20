99e9e1d3a4074f10aab817af6223ba1fedcb662b
rutvij-fsd <rutvijpatel414@gmail.com>
2023-09-20 22:41:30 +0530

    Add Loader.tsx component for displaying progress
    - Introduced a new ProgressLoader component.
    - Utilized MUI's LinearProgress for a sleek loading indicator.
    - Wrapped the progress bar in a Box component for full-width display.

---
+++ src/component/Loader.tsx
@@ -0,0 +1,12 @@
+import { Box, LinearProgress } from '@mui/material';
+
+const ProgressLoader = () => {
+  return (
+    <Box sx={{ width: '100%' }}>
+      <LinearProgress />
+    </Box>
+
+  );
+};
+
+export default ProgressLoader;
